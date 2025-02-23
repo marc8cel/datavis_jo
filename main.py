b4fd8746bbc7d0d0a00cd417895c416529eb8fbb
marc8cel <marc.celerier.25@eigsi.fr>
2025-02-23 15:52:02 +0100

    Améliorations de l'affichage et navigation par sport/événement
    - Ajout d'une barre de recherche pour filtrer par catégorie
    - Ajout d'une barre de navigation en haut avec le logo des Jeux Olympiques pour revenir au menu
    - Réduction du nombre de catégories à 12 et regroupement de certains sports
    - Affichage des événements associés lorsqu'on clique sur un sport
    - Affichage du sport et de l'événement sélectionné en haut de l'écran
    - Ajout d'un fichier CSV contenant les drapeaux des pays (préparation pour affichage futur)
    
    Prochaine étape :
    - Récupérer les résultats depuis les fichiers CSV de Kaggle
    - Associer les résultats aux événements sélectionnés pour afficher un podium

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def load_sports_data():
             categories[category] = {}
 
         if sport not in categories[category]:
-            categories[category][sport] = []
+            categories[category][sport] = {"events": []}
 
-        categories[category][sport].append(event)
+        categories[category][sport]["events"].append(event)
 
     # Trier les sports et les événements par ordre alphabétique
     for category in categories:
         categories[category] = dict(sorted(categories[category].items()))
         for sport in categories[category]:
-            categories[category][sport].sort()
+            categories[category][sport]["events"].sort()
 
     return categories
 
@@ -35,10 +35,11 @@ def home():
 
     first_three_sports = {}
     remaining_sports = {}
+
     for category, sports in categories.items():
-        sorted_sports = sorted(sports.keys())  # Trier les clés (sports) par ordre alphabétique
-        first_three_sports[category] = sorted_sports[:3]
-        remaining_sports[category] = sorted_sports[3:]
+        sorted_sports = sorted(sports.keys())  # Trier les sports par ordre alphabétique
+        first_three_sports[category] = {sport: sports[sport] for sport in sorted_sports[:3]}
+        remaining_sports[category] = {sport: sports[sport] for sport in sorted_sports[3:]}
 
     return render_template("filter_sport.html", first_three_sports=first_three_sports, remaining_sports=remaining_sports)
 
